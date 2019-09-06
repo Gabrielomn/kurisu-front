4aac0ebb2c3cea2ee90cbe8f7b57f15733da6b96
gabrielomn <gabriel.nobrega@ccc.ufcg.edu.br>
2019-09-06 15:55:47 -0300

    adding assets

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {ReactComponent as HomeImg} from '../../assets/svg/homeImg.svg'
+import {ReactComponent as GitHubImg} from '../../assets/svg/github.svg'
 import './Home.css'
 
 export class Home extends React.Component{
@@ -6,8 +8,19 @@ export class Home extends React.Component{
 
     render (){
         return <div className="Home">
-                    <i>placeholder</i>
+                    <HomeImg></HomeImg>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, amet omnis exercitationem sequi nesciunt vel eaque deserunt reprehenderit adipisci quod? Labore dicta temporibus necessitatibus tempora repellat tenetur consequuntur enim nemo.</p>
-               </div>
+                        <h3>Desenvolvedores</h3>
+                    <div className="devs">
+                        <div className="dev">
+                            <h3>Gabriel Nóbrega</h3>
+                            <GitHubImg></GitHubImg>
+                        </div>
+                        <div className="dev">
+                            <h3>João Felipe</h3>
+                            <GitHubImg></GitHubImg>
+                        </div>
+                    </div>
+                </div>
     }
 }
